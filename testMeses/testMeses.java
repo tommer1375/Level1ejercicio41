ff298a6623991e2f79687d6a391f1223f8f52909
Jorge Monzó <151056618+tommer1375@users.noreply.github.com>
2024-01-29 13:36:37 +0100

    Update testMeses.java

--- testMeses/testMeses.java
+++ testMeses/testMeses.java
@@ -4,16 +4,24 @@ import java.util.*;
 
 class testMeses {
 
-    @Test
-    public void testNumeroMeses() {
-
         n4ejercicio1 lecturaMeses = new n4ejercicio1();
         ArrayList<String> monthList = lecturaMeses.getMonthList();
+    
+    @Test
+    public void Testsize() {
 
-        assertEquals(12, monthList.size());
+        assertEquals(12, monthList.size(), "hay 12 posiciones");
+    }
+    @Test
+    public void testElementvoid () {
+        assertNotEquals(0, monthList.size(), "Comprobar que no hay 0 elementos");
 
-        assertFalse(monthList.isEmpty());
+    }
+    @Test
+    public void testAgost(){
+        assertEquals(monthList.get(7), "agost","Comprobar que agost está en la posición correcta");
 
-        assertEquals("agost", monthList.get(7));
+
+    }
     }
 }
